59827bb812c8da1ce19e24b6d4dc414879193046
cristideveloper <46890664+cristideveloper@users.noreply.github.com>
2019-02-19 16:02:31 +0200

    Update tema3rev.pl

--- TEMA 3/tema3rev.pl
+++ TEMA 3/tema3rev.pl
@@ -10,16 +10,10 @@ use Destination;
 my @array = new Source("test_perl_3.csv",";");
 my $index = scalar @array;
 
-
-#my @header = Source->get_header();
-#my $row1 = Source->next_row();
-#my $row2 = Source->next_row();
-
 new Destination("new_test_perl_3.csv",";");
 
 Destination->add_header(Source->get_header());
 $index--;
-
 
 while ($index){
 	Destination->add_row(Source->next_row());
@@ -28,11 +22,4 @@ while ($index){
 
 Destination->print_columns_to_csv_in_order();
 
-#print Dumper @array;
-#print Dumper $header;
-#print Dumper $head;
-#print Dumper $header;
 
-#print Dumper $row1;
-
-#print Dumper $row2;
